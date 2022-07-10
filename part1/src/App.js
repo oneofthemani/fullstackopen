9a244c6eecba89b01e913aae9f2f7e187a3d96a3
Kaan <kaankor999@gmail.com>
2022-07-10 22:10:55 +0200

    The learning part of the component state

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//and this is called destructuring
+const Hello = ({ name, age }) => {
+  //const name = props.name; //for more comfort
+  //const age = props.age; 
+  //For more comfort, it is called destructuring
+
+  //const { name, age }  = props;
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear;
+    return yearNow - props.age;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
-  );
+  )
+}
+
+const App = (props) => {
+  const { counter } = props;
+  return (
+    <div>{counter}</div>
+  )
 }
 
 export default App;
